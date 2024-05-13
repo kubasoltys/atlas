b72e9365bc5e6dcd04e8aaaed43685765cdf0699
kubasoltys <120708238+kubasoltys@users.noreply.github.com>
2024-05-13 11:33:58 +0200

    Add files via upload

--- atlas.js
+++ atlas.js
@@ -6,17 +6,23 @@ fetch('https://restcountries.com/v3.1/region/europe')
         console.log(data);
         data.forEach(stat => {
             let blockCountry = 
-            `<div class="col-xl-2 col-lg-3 col-md-6 col-sm-8 container">
-                <div class="card">
-                    <a href="${stat.maps.googleMaps}"></a>
-                    <img src="${stat.flags.png}" alt="${stat.name.official}" class="card-img-top">
+            `<div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
+                <div class="card mb-4" style="width: 18rem;">
                     <div class="card-body">
-                        <h4 class="card-title">${stat.translations.ces.common}</h4>
-                        <p class="card-text">Počet obyvatel: ${stat.population}
-                            <br>Rozloha: ${stat.area} km<sup>2</sup>
+                        <a href="${stat.maps.googleMaps}">
+                            <img src="${stat.flags.png}" alt="${stat.name.official}" class="card-img-top rounded">
+                        </a>
+                        <div class="text-center">
+                            <h3 class="card-title">${stat.name.common}</h3>
+                            <h5 class="card-subtitle mb-2 text-muted">${stat.subregion}</h5>
+                        </div>
+                        <p class="card-text mt-3">
+                            <b>Capital city:</b> ${stat.capital}
+                        <br><b>Population:</b> ${stat.population}
+                        <br><b>Area:</b> ${stat.area} km<sup>2</sup>
                         </p>
                     </div>
-                </div>
+                </div>    
             </div>`;
             staty.innerHTML += blockCountry;
         });
